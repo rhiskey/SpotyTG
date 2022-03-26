1ca3bb3af464a0559da9591f71a7a04824107bfa
Vladimir Kiselev <first3395@gmail.com>
2022-03-27 00:25:01 +0300

    Badges

---
+++ tests/test_track_unit_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+
+func TestTrack(t *testing.T) {
+	//ProcessUrl(n, playlistURL, update, msg)
+
+	//if total != 10 {
+	//	t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
+	//}
+}
